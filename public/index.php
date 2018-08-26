a39137fcc5f50e978305322f6ad751c77a5b33ad
damirTV <31268720+damirTV@users.noreply.github.com>
2018-08-26 20:46:11 +0300

    ДЗ Урок-2

--- public/index.php
+++ public/index.php
@@ -4,6 +4,12 @@ include '../classes/Units.Product.class.php';
 include '../classes/Electronic.Product.class.php';
 include '../classes/Weight.Product.class.php';
 
+// function __autoload($class_name)
+// {
+//     $filename = '../classes/' . $class_name . 'Product.class.php';
+//     include $filename;
+// }
+
 $nuts = new Units();
 $nuts->setPrice(100); // цена штучного товара 100 рублей
 echo 'Цена штучного товара: ' . $nuts->getPrice();
@@ -17,4 +23,3 @@ echo '<br>';
 $banan = new Weight();
 $banan->setPrice(2); // продается 2 кг
 echo 'Цена весового товара: ' . $banan->getPrice();
-
